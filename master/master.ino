746eff955d05f6fbc41a6249ef0ead5d0cc2bfb2
SmarboS <smarbos@gmail.com>
2022-06-25 15:03:17 -0300

    wip

--- master/master.ino
+++ master/master.ino
@@ -11,13 +11,14 @@ char keys[ROWS][COLS] = {
   {'7','8','9','C'},
   {'#','0','*','D'}
 };
-byte rowPins[ROWS] = {2, 3, 4, 5}; //connect to the row pinouts of the keypad
-byte colPins[COLS] = {6, 7, 8, 9}; //connect to the column pinouts of the keypad
-
+byte rowPins[ROWS] = {9, 8, 7, 6}; //connect to the row pinouts of the keypad
+byte colPins[COLS] = {5, 4, 3, 2}; //connect to the column pinouts of the keypad
+char target[2] = {'*', '*'};
+char *controller;  
+bool numberInput = false;
 Keypad keypad = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
 
 
- char *controller;  
  int buttonState1 = 0;  
  void setup()   
  {  
@@ -30,26 +31,57 @@ Keypad keypad = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
  delay(100);}  
  void loop()  
  {  
+
+  
    char key = keypad.getKey();
 
   if (key != NO_KEY){
+    if(numberInput == false && target[0] == '*' && key != 'D') {
+      target[0] = key;
+      Serial.print("[0][");
+      Serial.print(key);
+      Serial.println("]");
+      numberInput = true;
+    }
+    if(numberInput == false && target[1] == '*' && key != 'D') {
+      target[1] = key;
+      Serial.print("[1][");
+      Serial.print(key);
+      Serial.println("]");
+      numberInput = true;
+    }
+    Serial.println("------------");
+    Serial.println(numberInput);
+    Serial.println(target[0]);
+    Serial.println(target[1]);
     Serial.println(key);
-    buttonState1 = 1;
-  } else {
-    buttonState1 = 0;
+    Serial.println("------------");
+    if (target[0] != '*' && target[1] != '*' && key == 'D') {
+      Serial.print("Llamando #");
+      Serial.print(target[0]);
+      Serial.println(target[1]);
+      
+      vw_send((uint8_t *)target, strlen(target)+1);  
+   vw_wait_tx(); // Wait until the whole message is gone 
+   digitalWrite(10,HIGH);  
+   delay(500);
+   controller=0 ;  
+            vw_send((uint8_t *)controller, strlen(controller)+1);  
+    vw_wait_tx(); // Wait until the whole message is gone  
+    digitalWrite(10, LOW);  
+   digitalWrite(10, LOW);  
+      target[0] =  '*';
+      target[1] =  '*';
+      numberInput = true;
+      } else {
+        controller=0 ;  
+            vw_send((uint8_t *)controller, strlen(controller));  
+    vw_wait_tx(); // Wait until the whole message is gone  
+    digitalWrite(10, LOW);  
+      }
+    
+    numberInput = false;
   }
   // Serial.println(controller);  
-  if (buttonState1 == 1)  
- {  
-   vw_send((uint8_t *)key, strlen(key));  
-   vw_wait_tx(); // Wait until the whole message is gone  
-   digitalWrite(10,LOW);  
-   }  
-  else  
-  {  
-  controller="0" ;  
- vw_send((uint8_t *)controller, strlen(controller));  
- vw_wait_tx(); // Wait until the whole message is gone  
- digitalWrite(10, HIGH);  
-  }  
+  
  }  
--- slave/slave.ino
+++ slave/slave.ino
@@ -1,28 +1,64 @@
- #include <VirtualWire.h>  
+ #include <VirtualWire.h>
+
+ bool active = false;
+ int greenTime = 0;
+ int statusLed = 11;
+ bool blink = false;
+ 
+ int timePassed (int time) {
+  int diff = 0;
+
+  if (millis() <= time) {
+    diff = (69666 - time) + millis();
+  } else {
+    diff = millis() - time;
+  }
+
+  return diff;
+}
+
  void setup()  
  {  
  vw_set_ptt_inverted(true); // Required for DR3100  
  vw_set_rx_pin(12);  
  vw_setup(4000); // Bits per sec  
  Serial.begin(9600);  
- pinMode(9, OUTPUT);  
+ pinMode(LED_BUILTIN, OUTPUT);  
+ pinMode(statusLed, OUTPUT); 
  vw_rx_start(); // Start the receiver PLL running  
  }  
+ 
  void loop()  
  {  
  uint8_t buf[VW_MAX_MESSAGE_LEN];  
- uint8_t buflen = VW_MAX_MESSAGE_LEN;  
+ uint8_t buflen = 2;  
     
  if (vw_get_message(buf, &buflen)) // Non-blocking  
  {  
-  Serial.println(buf[0]); 
- if(buf[0]==36)  
- {  
- digitalWrite(9,HIGH);  
- }   
- else   
- {  
- digitalWrite(9,LOW);  
- }  
- }  
- }  
+  String str = (char*)buf;
+  Serial.println(str); 
+  String new_str = str.substring(0, 2);
+  Serial.println(new_str); 
+   if(new_str=="08")  
+   {  
+    active = true;
+   }    
+ }
+ if (active) {
+    if (timePassed (greenTime) >= 500) {
+    switchGreen();
+    }
+ } else {
+    digitalWrite(LED_BUILTIN, LOW);  
+ }
+}
+
+void switchGreen () {
+  blink = !blink;
+  if (blink) {
+    digitalWrite(LED_BUILTIN, HIGH);  
+  } else {
+    digitalWrite(LED_BUILTIN,LOW); 
+  }
+  greenTime = millis();
+}
